ab38eb54954a4e00d11ba944b8b2b449ded4c53e
Chee Wai Wong <academicsnotes@gmail.com>
2024-03-29 02:16:39 +0800

    Commit scripts

--- src/test2.py
+++ src/test2.py
@@ -1,15 +1,14 @@
 import sqlite3
 
 def main():
-    db = sqlite3.connect("D:\git\python_database_practices\db\scratch.db")
-    cur = db.cursor()
-  
-    cur.execute("DROP TABLE IF EXISTS temp")
+    connection = sqlite3.connect("scratch.db")
+
+    cur = connection.cursor()
     cur.execute("CREATE TABLE IF NOT EXISTS temp ( a TEXT, b TEXT, c TEXT )")
     cur.execute("INSERT INTO temp VALUES('one','two','three')")
     cur.execute("INSERT INTO temp VALUES('four','five','six')")
     cur.execute("INSERT INTO temp VALUES('seven','eight','nine')")
-    db.commit()
+    # connection.commit()
 
     cur.execute("SELECT * FROM temp")
     row = cur.fetchone()
@@ -19,8 +18,10 @@ def main():
         print(row)
 
     cur.execute("DROP TABLE IF EXISTS temp")
-    cur.close()
-    db.close()
 
-    if __name__ =="__main__":
-        main()
+    cur.close()   
+    connection.commit()
+    connection.close()
+
+if __name__ =="__main__":
+    main()
